e72603231973354297cc3756eff4b31c3b28a0eb
Prateek Yadav <prateek246729@gmail.com>
2026-03-18 17:40:00 +0530

    update api url for production

--- employee-frontend/src/services/EmployeeService.js
+++ employee-frontend/src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api/employees';
+const API_BASE_URL = 'https://employee-management-uyra.onrender.com/api/employees';
 
 
 const EmployeeService = {
